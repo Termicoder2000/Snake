b25865417aa7eaeee9359d4d3ccaa491ad8f7c17
Nils Hepp <Nils.Hepp@bwedu.de>
2021-10-12 15:40:20 +0200

    jgfhgfjhfjfjfgh

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -8,7 +8,7 @@ public:
 	GameWindow()
 		: Window(800, 600)
 	{
-		set_caption("Gosu Tutorial mit Git");
+		set_caption("Gosu Tutorial mit Git Vorlesung Test nnn");
 	}
 
 	// Wird bis zu 60x pro Sekunde aufgerufen.
