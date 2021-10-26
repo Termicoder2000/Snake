d7f654d00982dec7a9a76fe8f0199b22be596fc0
Nils Hepp <Nils.Hepp@bwedu.de>
2021-10-26 13:58:14 +0200

    aktuell

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -237,7 +237,7 @@ public:
 		
 		// Spielfeldbegrenzung ++++++++++++++++++++++++++++++++
 		if (s.pos.at(0).x <= Spielbrettbegrenzung_x_min || s.pos.at(0).x > Spielbrettbegrenzung_x_max || s.pos.at(0).y <= Spielbrettbegrenzung_y_min || s.pos.at(0).y > Spielbrettbegrenzung_y_max) {
-		Window::close(); // Schlieﬂt nur, l‰sst nicht neu starten.
+			Window::close(); // Schlieﬂt nur, l‰sst nicht neu starten.
 		}
 
 		// Falls Schlange sich selbst essen will
@@ -248,7 +248,6 @@ public:
 				Window::close();
 			}
 		}
-
 	}
 };
 
